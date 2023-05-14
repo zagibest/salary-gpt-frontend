2bbfa6821abe9ca250b90b9df8cef792ca82d855
Zandan-Ochir Ganbold <zagig35@gmail.com>
2023-05-15 00:58:00 +0800

    ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,73 +1,56 @@
 import * as React from 'react';
+// import Seo from '@/components/Seo';
+import { TbSend } from 'react-icons/tb';
 
 import Layout from '@/components/layout/Layout';
-import ArrowLink from '@/components/links/ArrowLink';
-import ButtonLink from '@/components/links/ButtonLink';
-import UnderlineLink from '@/components/links/UnderlineLink';
-import UnstyledLink from '@/components/links/UnstyledLink';
-import Seo from '@/components/Seo';
-
-/**
- * SVGR Support
- * Caveat: No React Props Type.
- *
- * You can override the next-env if the type is important to you
- * @see https://stackoverflow.com/questions/68103844/how-to-override-next-js-svg-module-declaration
- */
-import Vercel from '~/svg/Vercel.svg';
-
-// !STARTERCONF -> Select !STARTERCONF and CMD + SHIFT + F
-// Before you begin editing, follow all comments with `STARTERCONF`,
-// to customize the default configuration.
 
 export default function HomePage() {
+  const [conversation, setConversation] = React.useState<string[]>([]);
   return (
     <Layout>
-      {/* <Seo templateTitle='Home' /> */}
-      <Seo />
-
-      <main>
-        <section className='bg-white'>
-          <div className='layout relative flex min-h-screen flex-col items-center justify-center py-12 text-center'>
-            <Vercel className='text-5xl' />
-            <h1 className='mt-4'>
-              Next.js + Tailwind CSS + TypeScript Starter
-            </h1>
-            <p className='mt-2 text-sm text-gray-800'>
-              A starter for Next.js, Tailwind CSS, and TypeScript with Absolute
-              Import, Seo, Link component, pre-configured with Husky{' '}
-            </p>
-            <p className='mt-2 text-sm text-gray-700'>
-              <ArrowLink href='https://github.com/theodorusclarence/ts-nextjs-tailwind-starter'>
-                See the repository
-              </ArrowLink>
-            </p>
-
-            <ButtonLink className='mt-6' href='/components' variant='light'>
-              See all components
-            </ButtonLink>
-
-            <UnstyledLink
-              href='https://vercel.com/new/git/external?repository-url=https%3A%2F%2Fgithub.com%2Ftheodorusclarence%2Fts-nextjs-tailwind-starter'
-              className='mt-4'
-            >
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img
-                width='92'
-                height='32'
-                src='https://vercel.com/button'
-                alt='Deploy with Vercel'
-              />
-            </UnstyledLink>
-
-            <footer className='absolute bottom-2 text-gray-700'>
-              © {new Date().getFullYear()} By{' '}
-              <UnderlineLink href='https://theodorusclarence.com?ref=tsnextstarter'>
-                Theodorus Clarence
-              </UnderlineLink>
-            </footer>
+      <main className='h-screen'>
+        <div className='flex min-h-screen bg-[#444654]'>
+          <div className=' flex h-screen w-[300px] flex-col bg-[#202123] p-4 text-white'>
+            SalaryGPT
           </div>
-        </section>
+          <div className='relative flex w-full flex-col'>
+            <div className='flex h-[80vh] flex-col overflow-y-auto px-4'>
+              {conversation.map((item: string, index: number) => (
+                <div
+                  key={index}
+                  className={`${
+                    index % 2 === 0 ? 'justify-start' : 'justify-end'
+                  } flex`}
+                >
+                  <div
+                    className={`${
+                      index % 2 === 0 ? 'bg-[#2d3036]' : 'bg-[#444654]'
+                    } my-2 rounded-md px-4 py-3 text-white`}
+                  >
+                    {item}
+                  </div>
+                </div>
+              ))}
+            </div>
+            <form
+              // eslint-disable-next-line @typescript-eslint/no-explicit-any
+              onSubmit={(e: any) => {
+                e.preventDefault();
+                setConversation([...conversation, e.target.promt.value]);
+              }}
+            >
+              <div className='absolute bottom-0 right-0 w-full bg-[#2d3036] px-4 py-10'>
+                <input
+                  className=' relative w-full rounded-md bg-[#40414f] px-4 py-3 text-white'
+                  name='promt'
+                />
+                <button className='absolute right-6 top-10 p-3' type='submit'>
+                  <TbSend size={24} className='text-[#d9d9e3]' />
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
       </main>
     </Layout>
   );
